83173cb77eba19d7460c76ce3feb45d82051cf16
David Aghassi <david.aghassi@gmail.com>
2016-11-16 22:19:28 -0500

    Started writing unit tests for Favorite class

---
+++ app/src/test/java/com/findmybusnj/findmybusnj/FavoriteTest.java
@@ -0,0 +1,57 @@
+package com.findmybusnj.findmybusnj;
+
+import com.findmybusnj.findmybusnj.models.Favorite;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class FavoriteTest {
+    Favorite favorite;
+
+    @Before
+    public void setUp() {
+        favorite = new Favorite();
+    }
+
+    @After
+    public void teardown() {
+        favorite = null;
+    }
+
+    @Test
+    public void assertPrimaryKeyWithoutRoute() {
+        String expected = "11111";
+        favorite.setStop(expected);
+
+        assertThat("Primary key equals stop", favorite.generatePrimaryKey().equals(expected), is(true));
+    }
+
+    @Test
+    public void assertPrimaryKeyWithRoute() {
+        String stop = "11111";
+        String route = "222";
+        String expected = stop + route;
+
+        favorite.setStop(stop);
+        favorite.setRoute(route);
+
+        assertThat("Primary key equals stop + route", favorite.generatePrimaryKey().equals(expected), is(true));
+    }
+
+    @Test
+    public void assertIncrementsFrequency() {
+        favorite.setFrequency(0);
+        favorite.incrementFrequency();
+
+        assertThat("Frequency incremented by 1", favorite.getFrequency() == 1, is(true));
+    }
+}
