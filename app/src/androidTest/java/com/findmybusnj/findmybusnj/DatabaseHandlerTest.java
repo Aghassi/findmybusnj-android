1259c9658ec4969b3f30f4e0d514f9875470882c
David Aghassi <david.aghassi@gmail.com>
2016-12-09 15:44:34 -0500

    Initial attempt at database testing (doesn't work)

--- app/src/androidTest/java/com/findmybusnj/findmybusnj/DatabaseHandlerTest.java
+++ app/src/androidTest/java/com/findmybusnj/findmybusnj/DatabaseHandlerTest.java
@@ -4,9 +4,13 @@ import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
+import com.findmybusnj.findmybusnj.handlers.DatabaseHandler;
+import com.findmybusnj.findmybusnj.models.Favorite;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 /**
@@ -15,12 +19,22 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
+public class DatabaseHandlerTest {
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("com.findmybusnj.findmybusnj", appContext.getPackageName());
+    }
+
+    @Test
+    public void testInsertionOfFavorite() throws Exception {
+        Favorite favorite = new Favorite("", "", 0);
+        favorite.setStop("26229");
+
+        DatabaseHandler db = new DatabaseHandler(InstrumentationRegistry.getContext());
+        db.addFavorite(favorite);
+        assertThat("Favorite was inserted into the database", db.getFavoritesCount() == 1, is(true));
     }
 }
